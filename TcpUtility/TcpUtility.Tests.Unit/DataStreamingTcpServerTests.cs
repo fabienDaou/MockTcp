98b724041471ea64a09f5a59dae9da2ccc516bc5
Fabien Daoulas <fabien.daoulas@gmail.com>
2017-05-23 22:10:19 +0200

    Add unit tests for Start/Stop methods of DataStreamingTcpServer.

--- TcpUtility/TcpUtility.Tests.Unit/DataStreamingTcpServerTests.cs
+++ TcpUtility/TcpUtility.Tests.Unit/DataStreamingTcpServerTests.cs
@@ -26,13 +26,91 @@ namespace TcpUtility.Tests.Unit
                 var dataStreamingTcpServer = new DataStreamingTcpServer(0);
 
                 // Assert
-                TcpServer tcpServer = GetTcpServerInstanceByReflection(dataStreamingTcpServer);
-                var acceptedClientHandler = typeof(TcpServer).GetField(nameof(tcpServer.AcceptedClient), BindingFlags.NonPublic | BindingFlags.Instance).GetValue(tcpServer) as Delegate;
+                context.Verify();
+            });
+        }
+
+        [Test]
+        public void Start_StartCalled_DataStreamingTcpServerSubscribedToAcceptedClient()
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var tcpListenerMock = new Mock<ITcpListener>();
+
+
+                context.Setup(() => MultiSourceTcpListener.Create(It.IsAny<int>()))
+                    .Returns(tcpListenerMock.Object);
+
+                // Act
+                var dataStreamingTcpServer = new DataStreamingTcpServer(0);
+                dataStreamingTcpServer.Start();
+
+                // Assert
+                var acceptedClientHandler = GetAcceptedClientHandlerByReflection(dataStreamingTcpServer);
 
                 Assert.IsNotNull(acceptedClientHandler);
                 Assert.AreEqual(1, acceptedClientHandler.GetInvocationList().Length);
-                context.Verify();
             });
+        }
+
+        [Test]
+        public void Start_CallTwice_DoesNotThrow()
+        {
+            // Arrange
+            var dataStreamingTcpServer = new DataStreamingTcpServer(0);
+
+            // Assert
+            Assert.DoesNotThrow(() =>
+            {
+                dataStreamingTcpServer.Start();
+                dataStreamingTcpServer.Start();
+            });
+        }
+
+        [Test]
+        public void Stop_StopCalled_DataStreamingTcpServerUnsubscribedFromAcceptedClient()
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var tcpListenerMock = new Mock<ITcpListener>();
+
+                context.Setup(() => MultiSourceTcpListener.Create(It.IsAny<int>()))
+                    .Returns(tcpListenerMock.Object);
+
+                var dataStreamingTcpServer = new DataStreamingTcpServer(0);
+                dataStreamingTcpServer.Start();
+
+                // Act
+                dataStreamingTcpServer.Stop();
+
+                // Assert
+                var acceptedClientHandler = GetAcceptedClientHandlerByReflection(dataStreamingTcpServer);
+
+                Assert.IsNull(acceptedClientHandler);
+            });
+        }
+
+        [Test]
+        public void Stop_CallTwice_DoesNotThrow()
+        {
+            // Arrange
+            var dataStreamingTcpServer = new DataStreamingTcpServer(0);
+            dataStreamingTcpServer.Start();
+
+            // Assert
+            Assert.DoesNotThrow(() =>
+            {
+                dataStreamingTcpServer.Stop();
+                dataStreamingTcpServer.Stop();
+            });
+        }
+
+        private Delegate GetAcceptedClientHandlerByReflection(DataStreamingTcpServer dataStreamingTcpServer)
+        {
+            TcpServer tcpServer = GetTcpServerInstanceByReflection(dataStreamingTcpServer);
+            return typeof(TcpServer).GetField(nameof(tcpServer.AcceptedClient), BindingFlags.NonPublic | BindingFlags.Instance).GetValue(tcpServer) as Delegate;
         }
 
         private TcpServer GetTcpServerInstanceByReflection(DataStreamingTcpServer dataStreamingTcpServer)
