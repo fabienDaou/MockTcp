d4df219414edf4c259d7852fcea70cf43edb31b7
Fabien Daoulas <fabien.daoulas@gmail.com>
2017-05-23 21:34:23 +0200

    Add unit test for the constructor of DataStreamingTcpServer.

---
+++ TcpUtility/TcpUtility.Tests.Unit/DataStreamingTcpServerTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using Smocks;
+using System;
+using System.Reflection;
+
+namespace TcpUtility.Tests.Unit
+{
+    [TestFixture]
+    public class DataStreamingTcpServerTests
+    {
+        [Test]
+        public void Ctor_ConstructorCalled_TcpServerInstanciated()
+        {
+            Smock.Run(context =>
+            {
+                // Arrange
+                var tcpListenerMock = new Mock<ITcpListener>();
+
+                
+                context.Setup(() => MultiSourceTcpListener.Create(It.IsAny<int>()))
+                    .Returns(tcpListenerMock.Object)
+                    .Verifiable();
+                
+                // Act
+                var dataStreamingTcpServer = new DataStreamingTcpServer(0);
+
+                // Assert
+                TcpServer tcpServer = GetTcpServerInstanceByReflection(dataStreamingTcpServer);
+                var acceptedClientHandler = typeof(TcpServer).GetField(nameof(tcpServer.AcceptedClient), BindingFlags.NonPublic | BindingFlags.Instance).GetValue(tcpServer) as Delegate;
+
+                Assert.IsNotNull(acceptedClientHandler);
+                Assert.AreEqual(1, acceptedClientHandler.GetInvocationList().Length);
+                context.Verify();
+            });
+        }
+
+        private TcpServer GetTcpServerInstanceByReflection(DataStreamingTcpServer dataStreamingTcpServer)
+        {
+            return (TcpServer)typeof(DataStreamingTcpServer).GetField("tcpServer", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(dataStreamingTcpServer);
+        }
+    }
+}
